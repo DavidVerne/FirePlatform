ab86f3abcd77281d58d82917852eaf5f26ef8945
davidverne-jisc <david.verne@jisc.ac.uk>
2023-10-09 22:26:41 +0100

    Update index.js

--- ui/src/landing/Landing/index.js
+++ ui/src/landing/Landing/index.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import Header from './Header';
 import Home from './Home';
 import Footer from './Footer';
